b71a7cb7d9d428af23d0466229b65b81ba4556c2
Francisco Silva <francisjssilva@gmail.com>
2020-02-19 17:49:56 +0000

    [FEAT] Project structure begin

---
+++ Assets/Scripts/PhysicsInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PhysicsInfo : MonoBehaviour
+{
+    public float speed;
+    public float angularSpeed;
+    protected Rigidbody r;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = r.velocity.magnitude;
+        angularSpeed = r.angularVelocity.magnitude;
+    }
+}
